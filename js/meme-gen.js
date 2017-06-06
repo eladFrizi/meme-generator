03ebe0af79a8e0599e22a9598809aa78588c91c8
eladFrizi <okc.elad.35@gmail.com>
2017-06-06 20:46:48 +0300

    drag and drop

--- js/meme-gen.js
+++ js/meme-gen.js
@@ -8,6 +8,7 @@ function imgClicked(id, event) {
     var imgObj = findImageObJ(id);
     getCanvasState(imgObj)
     createCanvasWithImage(gCanvasState)
+    renderTxts(gCanvasState)
     showCard('meme-generator');
 }
 
@@ -25,36 +26,120 @@ function getCanvasState(imgObj) {
         text: 'edit me text1',
         fillStyle: 'red',
         textAlign: 'center',
+        left: 0,
+        top: 100,
+        size: 30,
+        fontFamily: 'fantasy'
+    }
+    gCanvasState.text2 = {
+        text: 'edit me text2',
+        fillStyle: 'red',
+        textAlign: 'left',
         left: 100,
-        top: 50,
-        size: '30px',
-        fontFamily: 'Comic'
+        top: 200,
+        size: 30,
+        fontFamily: 'fantasy'
     }
 }
 
-    function createCanvasWithImage(canvasState) {
-        var elImg = new Image();
-        elImg.src = canvasState.img.url;
-        var canvasSTR = `<canvas id="myCanvas" width="${elImg.width}" height="${elImg.height}" style="border:1px solid #d3d3d3;"
-         ondrop="drop_handler(event)">`
-        document.querySelector('.canvas-container').innerHTML = canvasSTR;
-        var c = document.getElementById("myCanvas");
-        var ctx = c.getContext("2d");
-        ctx.drawImage(elImg, 0, 0);
-        ctx.font = canvasState.text1.size + ' ' + canvasState.text1.fontFamily;
-        ctx.fillStyle = canvasState.text1.fillStyle;
-        ctx.textAlign = canvasState.text1.textAlign;
-        ctx.fillText(canvasState.text1.text, canvasState.text1.left, canvasState.text1.top);
+function createCanvasWithImage(canvasState) {
+    var elImg = new Image();
+    elImg.src = canvasState.img.url;
+    var canvasSTR = `<canvas id="myCanvas" width="${elImg.width}" height="${elImg.height}" style="border:1px solid #d3d3d3;">`
+    document.querySelector('.canvas-container').innerHTML = canvasSTR;
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+    ctx.drawImage(elImg, 0, 0);
+
+    console.log(canvasSTR)
+}
+
+
+function editText(num, input) {
+    var textKey = 'text' + num;
+    var value = input.value;
+    console.log(textKey);
+    gCanvasState[textKey].text = value;
+    renderTxts(gCanvasState)
+}
+
+
+// function renderTxts(state) {
+//     createCanvasWithImage(state)
+//     for (prop in state) {
+//         var checker = prop.substring(0, 4)
+//         if (checker === 'text') {
+//             var c = document.getElementById("myCanvas");
+//             var textKey = state[prop];
+//             var ctx = c.getContext("2d");
+//             ctx.font = textKey.size + 'px ' + textKey.fontFamily;
+//             ctx.fillStyle = textKey.fillStyle;
+//             ctx.textAlign = textKey.textAlign;
+//             ctx.fillText(textKey.text , textKey.left + textKey.size, textKey.top + textKey.size);
+//         }
+//     }
+// }
+
+
+function renderTxts(state) {
+    createCanvasWithImage(state)
+    for (prop in state) {
+        var checker = prop.substring(0, 4)
+        if (checker === 'text') {
+            var canvasCover = document.querySelector(".canvas-container");
+            var textKey = state[prop];
+            var divOverCanvas = document.createElement('div');
+            divOverCanvas.className = prop;
+            divOverCanvas.innerText = textKey.text;
+            divOverCanvas.style.maxWidth = canvasCover.width;
+            divOverCanvas.style.position = 'absolute';
+            divOverCanvas.style.width = '100%';
+            divOverCanvas.style.overflowWrap = 'break-word';
+            divOverCanvas.style.top = textKey.top + 'px';
+            divOverCanvas.style.fontSize = textKey.size + 'px';
+            divOverCanvas.style.textAlign = textKey.textAlign;
+            divOverCanvas.style.fontFamily = textKey.fontFamily;
+            divOverCanvas.style.zIndex = 50;
+            divOverCanvas.setAttribute('ondragstart', "drag(event)")
+            divOverCanvas.setAttribute('draggable', "true")
+            canvasCover.appendChild(divOverCanvas)
+        }
     }
-
-
-    function editText(num, input){
-        var textKey = 'text' + num;
-        var value = input.value;
-        console.log(textKey);
-        gCanvasState[textKey].text = value;
-        createCanvasWithImage(gCanvasState);
-    }
+}
 
 
 
+function editTextSize(num, input) {
+    var textKey = 'text' + num;
+    var value = input.value;
+    console.log(value);
+    gCanvasState[textKey].size = value;
+    renderTxts(gCanvasState)
+}
+
+
+
+
+
+function allowDrop(event) {
+    event.preventDefault();
+}
+
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.className);
+    console.log(ev.target.className)
+}
+
+function drop(ev) {
+    // var cardOfSetTop = document.querySelector('.meme-generator').offsetTop;
+    ev.preventDefault();
+    console.log(ev.target)
+    var data = ev.dataTransfer.getData("text");
+    var newTop = (ev.pageY - ev.target.offsetTop) + 'px';
+    console.log(newTop)
+    console.log(ev.target.offsetTop);
+    console.log(ev.pageY)
+    document.querySelector('.'+data).style.top = newTop;
+    ev.target.parentElement.appendChild(document.querySelector('.'+data));
+}
