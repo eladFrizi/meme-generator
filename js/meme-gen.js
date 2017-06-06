6050b767492bf619ae0dc798a78c2a1df0c5785f
eladFrizi <okc.elad.35@gmail.com>
2017-06-06 16:49:36 +0300

    bla

--- js/meme-gen.js
+++ js/meme-gen.js
@@ -36,7 +36,7 @@ function getCanvasState(imgObj) {
         var elImg = new Image();
         elImg.src = canvasState.img.url;
         var canvasSTR = `<canvas id="myCanvas" width="${elImg.width}" height="${elImg.height}" style="border:1px solid #d3d3d3;"
-        ondrop="drop(event)" ondragover="allowDrop(event)" >`
+         ondrop="drop_handler(event)">`
         document.querySelector('.canvas-container').innerHTML = canvasSTR;
         var c = document.getElementById("myCanvas");
         var ctx = c.getContext("2d");
@@ -57,26 +57,4 @@ function getCanvasState(imgObj) {
     }
 
 
-
-function drag_start(event) {
-    var style = window.getComputedStyle(event.target, null);
-    event.dataTransfer.setData("text/plain",
-    (parseInt(style.getPropertyValue("left"),10) - event.clientX) + ',' + (parseInt(style.getPropertyValue("top"),10) - event.clientY));
-} 
-function drag_over(event) { 
-    event.preventDefault(); 
-    return false; 
-} 
-function drop(event) { 
-    var offset = event.dataTransfer.getData("text/plain").split(',');
-    var dm = document.getElementById('dragme');
-    dm.style.left = (event.clientX + parseInt(offset[0],10)) + 'px';
-    dm.style.top = (event.clientY + parseInt(offset[1],10)) + 'px';
-    event.preventDefault();
-    return false;
-} 
-var dm = document.getElementById('dragme'); 
-dm.addEventListener('dragstart',drag_start,false); 
-document.body.addEventListener('dragover',drag_over,false); 
-document.body.addEventListener('drop',drop,false); 
 
