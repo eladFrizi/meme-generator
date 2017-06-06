199a5852c733191c5fde9441c2a970c64045047f
Snir Shechter <snirshechter@gmail.com>
2017-06-06 15:07:18 +0300

    finished img search box

--- js/image-picker.js
+++ js/image-picker.js
@@ -1,11 +1,10 @@
 'use strict';
 
-function displayImgs(imgs,imgWidth) {
+function displayImgs(imgs, imgWidth) {
     var elImageContainer = document.querySelector('.image-container');
     elImageContainer.innerHTML = '';
-    var imgCountOfLine = Math.floor((document.body.offsetWidth*90)/100 / imgWidth);
+    var imgCountOfLine = Math.floor((document.body.offsetWidth * 90) / 100 / imgWidth);
     var count = 0;
-    var isEndingWithSmallLine = false;
     for (var i = 0; i < imgs.length; i++) {
         if (count === (imgCountOfLine * 2) - 1) {
             count = 0;
@@ -13,9 +12,56 @@ function displayImgs(imgs,imgWidth) {
         }
         var elImg = document.createElement('img');
         elImg.setAttribute('src', imgs[i].url);
-        elImg.setAttribute('onclick','generateMeme(this)');
+        elImg.setAttribute('onclick', 'generateMeme(this)');
         elImageContainer.appendChild(elImg);
-        if(count === imgCountOfLine) elImg.style.marginLeft = '65px'; 
+        if (count === imgCountOfLine) elImg.style.marginLeft = '65px';
         count++;
     }
 }
+
+function searchIfEnter(event, searchInput) {
+    if (event.keyCode === 13) searchInput.nextSibling.click();
+}
+function searchImgs(btn) {
+    var searchResults = [];
+
+    var searchTxtArr = btn.previousSibling.value.toLowerCase().split(' ');
+    btn.previousSibling.value = '';
+
+    for (var i = 0; i < gImgs.length; i++) { // iterating imgs
+        var img = gImgs[i];
+        for (var j = 0; j < searchTxtArr.length; j++) { // iterating txt keywords
+            var currKeyword = searchTxtArr[j];
+            for (var k = 0; k < img.keywords.length; k++) { // iterating img keywords
+                var imgKeyword = img.keywords[k];
+                if (currKeyword === imgKeyword) {
+                    searchResults = updateSearchResults(searchResults, i, searchTxtArr.length-j);
+                }
+            }
+        }
+    }
+    displayImgs(searchResults,110);
+}
+function updateSearchResults(searchResults, imgIdx, score) {
+    for (var k = 0; k < searchResults.length; k++) { // iterating search results
+        if (searchResults[k].id === gImgs[imgIdx].id) { // if img is found in matching imgs
+            searchResults[k].matchScore += score;
+            break;
+        }
+    }
+    if (k === searchResults.length) { // if img is not found in matching imgs
+        searchResults.push(gImgs.slice(imgIdx, imgIdx+1)[0]); // slicing img into search results
+        searchResults[searchResults.length - 1].matchScore = score;
+    }
+    return searchResults;
+}
+
+
+/*
+MATCH SCORES:
+for every single match:
+first word match = searchTxtArr.length
+last word match = 1
+
+NOTE:it is possible that an img will appear twice and get more score)
+ */
--- js/main.js
+++ js/main.js
@@ -1,21 +1,20 @@
 'use strict';
 
-var gImgs = [{ id: 1, url: "assets/meme/batman.png" ,txts:['batman','robin','bitchslap'] },
-{ id: 2, url: "assets/meme/cat.png",txts:['cat','face'] },
-{ id: 3, url: "assets/meme/di-caprio.png",txts:['di-caprio','cheers'] },
-{ id: 4, url: "assets/meme/dog.png",txts:['dog','face'] },
-{ id: 5, url: "assets/meme/facepalm.png",txts:['starwars','facepalm'] },
-{ id: 6, url: "assets/meme/history.png",txts:['history','teach'] },
-{ id: 7, url: "assets/meme/lord-of-the-rings.png",txts:['lotr','lordoftherings','boromir'] },
-{ id: 8, url: "assets/meme/nerd.png",txts:['nerd'] },
-{ id: 9, url: "assets/meme/skeleton.png",txts:['skeleton','dead'] },
-{ id: 10, url: "assets/meme/toy-story.png",txts:['toystory','woody','buzzlightyears','show'] }];
+var gImgs = [{ id: 1, url: "assets/meme/batman.png" ,keywords:['batman','robin','bitchslap'] },
+{ id: 2, url: "assets/meme/cat.png",keywords:['cat','face'] },
+{ id: 3, url: "assets/meme/di-caprio.png",keywords:['di-caprio','cheers'] },
+{ id: 4, url: "assets/meme/dog.png",keywords:['dog','face'] },
+{ id: 5, url: "assets/meme/facepalm.png",keywords:['starwars','facepalm'] },
+{ id: 6, url: "assets/meme/history.png",keywords:['history','teach'] },
+{ id: 7, url: "assets/meme/lord-of-the-rings.png",keywords:['lotr','lordoftherings','boromir'] },
+{ id: 8, url: "assets/meme/nerd.png",keywords:['nerd','face'] },
+{ id: 9, url: "assets/meme/skeleton.png",keywords:['skeleton','dead'] },
+{ id: 10, url: "assets/meme/toy-story.png",keywords:['toystory','woody','buzzlightyears'] }];
 
 //onload function
 function initApp() {
     displayImgs(gImgs,110);
     addEventListener('resize',function(){
-        console.log('RESIZE');
         displayImgs(gImgs,110);
         });
 }
